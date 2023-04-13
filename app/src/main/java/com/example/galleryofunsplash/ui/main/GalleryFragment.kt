cf13327712bc42d64447aa28b6aa9a74eea21190
Maksim.Kalitsev <maksim.kalitsev@ukr.net>
2023-04-13 14:31:47 +0300

    add change column count

--- app/src/main/java/com/example/galleryofunsplash/ui/main/GalleryFragment.kt
+++ app/src/main/java/com/example/galleryofunsplash/ui/main/GalleryFragment.kt
@@ -1,16 +1,18 @@
 package com.example.galleryofunsplash.ui.main
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.galleryofunsplash.R
 import com.example.galleryofunsplash.databinding.FragmentGalleryBinding
+import java.util.*
 
 
 enum class RequestState {
@@ -23,6 +25,8 @@ class GalleryFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel by viewModels<GalleryViewModel>()
     private lateinit var adapter: GalleryAdapter
+
+    private var columnCount = 2
 
     companion object {
         fun newInstance() = GalleryFragment()
@@ -50,14 +54,46 @@ class GalleryFragment : Fragment() {
         _binding = FragmentGalleryBinding.bind(view)
 
         adapter = GalleryAdapter()
-        val layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)//GridLayoutManager(requireContext(), 2)
+        val layoutManager = StaggeredGridLayoutManager(
+            2,
+            StaggeredGridLayoutManager.VERTICAL
+        )
         binding.rvGallery.layoutManager = layoutManager
         binding.rvGallery.adapter = adapter
 
         viewModel.photosLiveData.observe(viewLifecycleOwner, stateObserver)
         viewModel.getPhotos()
 
+        setupToolbarMenu()
     }
+
+    private fun setupToolbarMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+//            override fun onPrepareMenu(menu: Menu) {
+//                // Handle for example visibility of menu items
+//            }
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.toolbar_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                setColumnCount()
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun setColumnCount() {
+        columnCount++
+        if (columnCount > 4) {
+            columnCount = 2
+        }
+        val layoutManager =
+            StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL)
+        binding.rvGallery.layoutManager = layoutManager
+    }
+
 
     private val stateObserver = Observer<GalleryViewModel.State> { state ->
         binding.progressBar.isVisible = state.requestState == RequestState.LOADING
